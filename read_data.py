d1a4057a8a37e3721c29c5026812923a039abd21
Mayari Naresh <101977459+naresh-1388@users.noreply.github.com>
2026-04-25 21:54:27 +0530

    Remove data_overview function from read_data.py
    Removed the data_overview function and its calls.

--- read_data.py
+++ read_data.py
@@ -10,21 +10,9 @@ def data_read(path):
     df = pd.read_csv(path)
     return df
 
-def data_overview(df):
-    print("\n First 5 rows:")
-    print(df.head())
-
-    print("\n Dataset Info:")
-    print(df.info())
-
-    print("\n Summary:")
-    print(df.describe())
-
-
-
 
 if __name__ == "__main__":
     data = data_read(path)
     print(data)
 
-    data_overview(data)
+
